285aa13f4d95681f749a3bd4166c95756fda66bd
shresthagrawal <shresthagrawal.31@gmail.com>
2018-12-10 12:56:52 +0530

    added shell script for auto config

---
+++ IndividualTests/setup.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+rm -rf releaseAutomationEnv
+mkdir releaseAutomationEnv
+
+cd releaseAutomationEnv
+
+vagrant init 
+#vagrant box add fedora/29-cloud-base --force
+
+/bin/cat <<EOM >Vagrantfile
+Vagrant.configure("2") do |config|
+  config.vm.box = "fedora/29-cloud-base"
+
+  config.ssh.insert_key = false
+
+
+  config.vm.provision "ansible" do |ansible|
+    ansible.extra_vars = { ansible_python_interpreter: "/usr/bin/python3" }
+    ansible.playbook = "../$1"
+  end
+
+end
+EOM
+
+vagrant up
+
+#grep with regex
+HostName=$(vagrant ssh-config | grep 'HostName')
+HostName=${HostName#*'HostName '}
+Port=$(vagrant ssh-config | grep 'Port')
+Port=${Port#*'Port '}
+echo $HostName:$Port
+
+
+#Create an inventory manually 
+/bin/cat <<EOM >../vagrantInventory.ini
+[Vagrant]
+$HostName         ansible_port=$Port     ansible_user=vagrant     ansible_ssh_private_key_file='~/.vagrant.d/insecure_private_key'     ansible_python_interpreter=/usr/bin/python3
+EOM
+
+# to run the ansible 
+echo '##############################################################'
+echo 'Use either of the below methods to run the ansible playbook:'
+echo 'cd releaseAutomationEnv; vagrant provision;'
+echo "ansible-playbook $1 -i vagrantInventory.ini"
+#vagrant provision
+
